2f2f92ced96fb372e10a4890153ee93a9e3267a0
gmackie <graham.mackie@gmail.com>
2025-11-16 14:03:56 -0800

    contd

--- init.lua
+++ init.lua
@@ -3,4 +3,19 @@ require("user.commands")
 require("user.keymaps")
 require("user.plugin")
 
+require("catppuccin").setup({
+	flavour = "mocha",
+	integrations = {
+		cmp = true,
+		gitsigns = true,
+		nvimtree = true,
+		treesitter = true,
+		notify = true,
+		mini = {
+			enabled = true,
+			indentscope_color = "",
+		},
+	},
+})
+
 -- vim: ts=2 sts=2 sw=2 et
---
+++ lua/plugins/harpoon.lua
@@ -0,0 +1,46 @@
+return {
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local harpoon = require("harpoon")
+
+		-- REQUIRED
+		harpoon:setup()
+
+		-- Basic keymaps
+		vim.keymap.set("n", "<leader>a", function()
+			harpoon:list():add()
+		end, { desc = "Harpoon: Add file" })
+
+		vim.keymap.set("n", "<C-e>", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end, { desc = "Harpoon: Toggle quick menu" })
+
+		-- Quick navigation to specific files
+		vim.keymap.set("n", "<C-h>", function()
+			harpoon:list():select(1)
+		end, { desc = "Harpoon: Navigate to file 1" })
+
+		vim.keymap.set("n", "<C-t>", function()
+			harpoon:list():select(2)
+		end, { desc = "Harpoon: Navigate to file 2" })
+
+		vim.keymap.set("n", "<C-n>", function()
+			harpoon:list():select(3)
+		end, { desc = "Harpoon: Navigate to file 3" })
+
+		vim.keymap.set("n", "<C-s>", function()
+			harpoon:list():select(4)
+		end, { desc = "Harpoon: Navigate to file 4" })
+
+		-- Toggle previous & next buffers stored within Harpoon list
+		vim.keymap.set("n", "<C-S-P>", function()
+			harpoon:list():prev()
+		end, { desc = "Harpoon: Previous buffer" })
+
+		vim.keymap.set("n", "<C-S-N>", function()
+			harpoon:list():next()
+		end, { desc = "Harpoon: Next buffer" })
+	end,
+}
